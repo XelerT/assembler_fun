f5c8d83e20da46d98122e9b32ccbc7a422a54e5d
XelerT <taranov.sasha2004@gmail.com>
2023-03-04 10:27:25 +0300

    Added interuptions on keyboard. Version with working frame and register's stats.

--- intrptns/frame09.asm
+++ intrptns/frame09.asm
@@ -6,20 +6,38 @@ locals @@
 org 100h
 
 start:
-
-                mov ax, ds                              ; save ds of program
-                mov word [save_ds_in_buf - 2], ax       ; write ds in buffer
+                mov ax, ds                               ; save ds of program
+                mov word [save_ds_in_buf - 2],  ax       ; write ds in buffer in print_line
+                mov word [save_ds_in_buf1 - 2], ax       ; write ds in buffer in int 08h
+                mov word [save_ds_in_buf2 - 2], ax       ; write ds in buffer in int 09h
 
                 xor bx, bx
                 mov es, bx
 
-                mov bx, 4 * 9
+                mov bx, 4h * 8h
+
+                mov ax, es:[bx]
+                mov Old080fs, ax
+
+                cli
+                mov es:[bx], offset New08
+
+                mov ax, cs
+                mov di, es:[bx + 2]
+                mov Old080Seg, di
+                mov es:[bx + 2], ax
+
+                sti
+
+                ; start second interceptor
+
+                mov bx, 4h * 9h
 
                 mov ax, es:[bx]
                 mov Old090fs, ax
 
                 cli
-                mov es:[bx], offset New09
+                mov es:[bx], offset check_what2show_int09
 
                 mov ax, cs
                 mov di, es:[bx + 2]
@@ -28,67 +46,139 @@ start:
 
                 sti
 
-                ; int 9h
-
-next:
-                in al, 60h
-                cmp al, 4Fh     ; NumLk 1
-                jne next
-
                 mov ax, 3100h
                 mov dx, offset EOP
                 shr dx, 4h
                 inc dx
 
+                ; add dx, 8000d
+
                 int 21h
 
-New09           proc
+New08           proc
 
-                push ax bx cx es di si dx ds
+                push es ds
+                pusha
 
-                mov bx, 0B800h
+                db 0B8h                  ; mov ax, |
+                save_ds_in_buf1 dw 00h   ;         | [buffer]
+                mov ds, ax
+
+                mov bx, 0B800h                                          ; videomemory offset
                 mov es, bx
 
-                push dx bx cx
+                cmp word ptr ds: [SKIP_SHOWING_FRAME], 0h               ; check working mode
+                je @@SKIP_DRAWING_FRAME
 
-                push bx                                 ; save ds to print it's value
-                push ax                                 ; save ax to print it's value
 
-                mov bl, 68d                             ; x start position of frame
-                mov bh, 8d                             ; y start position of frame
+                mov bl, 68d                                             ; x start position of frame
+                mov bh, 8h                                              ; y start position of frame
 
-                mov ch, 10d                             ; height of frame
-                mov cl, 8d                             ; length of frame
+                mov ch, 0Ah                                             ; height of frame
+                mov cl, 8h                                              ; length of frame
 
-                mov si, offset FRAME_BUFFER             ; elements of frame
+                mov si, offset FRAME_BUFFER                             ; elements of frame
 
                 call print_frame
-;////////////////////////////////////////////////////////////////////////
-                mov bl, 70d                             ; print 'ax:'
-                mov bh, 10d
-                mov ch, 1d
+
+                mov word ptr ds: [SKIP_SHOWING_FRAME], 0h
+;////////////////////////////////////////////////////////////////////////////////////////////////////
+@@SKIP_DRAWING_FRAME:
+
+                cmp word ptr ds: [SKIP_SHOWING_REGS], 0h
+                je @@SKIP_DRAWING_REGS
+
+                mov bl, 70d
+                mov bh, 9h
+                mov ch, 1h
                 mov si, offset AX_LINE_BUF
-                call print_line
+                call print_line                         ; print 'ax:'
 
                 mov bl, 74d                             ; print ax in x
-                mov bh, 10d                             ; print ax in y
+                mov bh, 9h                              ; print ax in y
 
-                pop dx                                  ; dx = (original) ax
+                mov di, sp
+                mov dx, word ptr ss:[di + 0Ah]          ; dx = (original) ax
                 call print_hex
-;////////////////////////////////////////////////////////////////////////
+;//////////////////////////////////////////////////////////////////////////////////////////////////////
                 mov bl, 70d                             ; print 'bx:'
-                mov bh, 11d
-                mov ch, 1d
+                mov bh, 0Ah
+                mov ch, 1h
                 mov si, offset BX_LINE_BUF
                 call print_line
 
                 mov bl, 74d                             ; print bx in x
-                mov bh, 11d                             ; print bx in y
+                mov bh, 0Ah                             ; print bx in y
 
-                pop dx                                  ; bx = (original) bx
+                mov di, sp
+                mov dx, word ptr ss:[di + 6h]           ; dx = (original) bx
                 call print_hex
 
-                pop cx bx dx
+@@SKIP_DRAWING_REGS:
+
+                popa
+                pop ds es
+
+                db 0EAH
+
+Old080fs    dw 0
+Old080Seg   dw 0
+
+                endp
+
+;----------------------------------------------------------------------------------------------------
+;       Resident that get commands from keyboard
+;       Entry:
+;       Expects:
+;       Destroys:
+;       Exit:     Rewrite buffers with working modes
+;----------------------------------------------------------------------------------------------------
+
+check_what2show_int09  proc
+
+                push ax ds
+
+                db 0B8h                  ; mov ax, |
+                save_ds_in_buf2 dw 00h   ;         | [buffer]
+                mov ds, ax                                                 ; restore resident ds
+
+                xor ax, ax
+                in al, 60h                                                 ; get key
+
+                cmp ax, NumLk2                                             ; check on NumLk 2
+                jne @@skip1
+                xor word ptr ds: [SKIP_SHOWING_FRAME], NumLk2              ; change mode of drawing frame
+@@skip1:
+                xor ax, ax
+                in al, 60h                                                 ; get key
+
+                cmp ax, NumLk3                                             ; check on NumLk 3
+                jne @@skip2
+                xor word ptr ds: [SKIP_SHOWING_REGS], Numlk3               ; change mode of drawing registers
+@@skip2:
+                pop ds ax
+
+                call blink2keyboard
+
+                mov al, 20h                                                ; End of interuption
+                out 20h, al
+
+                db 0EAH                                                    ; jmp parent
+
+Old090fs    dw 0                                                           ; save position of parent
+Old090Seg   dw 0
+
+                endp
+
+;----------------------------------------------------------------------------------------------------
+;       Blinking to keyboard
+;       Entry:
+;       Expects:
+;       Destroys:
+;       Exit:           Signal to keyboard that we unpressed button
+;----------------------------------------------------------------------------------------------------
+
+blink2keyboard  proc
 
                 in al, 61h
                 or al, 80h
@@ -97,17 +187,7 @@ New09           proc
                 and al, not 80h
                 out 61h, al
 
-                mov al, 20h
-                out 20h, al
-
-                pop ds dx si di es cx bx ax
-
-                db 0EAH
-
-Old090fs    dw 0
-Old090Seg   dw 0
-
-                iret
+                ret
                 endp
 
 include ../frame_pr.asm
@@ -122,6 +202,15 @@ DI_LINE_BUF: db 'di:'
 SI_LINE_BUF: db 'si:'
 ES_LINE_BUF: db 'es:'
 DS_LINE_BUF: db 'ds:'
+
+SKIP_SHOWING_FRAME dw 0
+SKIP_SHOWING_REGS  dw 0
+
+NumLk1 = 4Fh
+NumLk2 = 50h
+NumLk3 = 51h
+
+; SCREEN_BUF: db
 
 EOP:
 
